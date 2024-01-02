012b04d9b4ec998f86063a0d9effe174bdca547f
Taisom Campos <taisomcampos@outlook.com>
2024-01-02 04:40:18 -0300

    Adicionando o desafio d002

---
+++ Desafios/d002/js/desafio-boas-vindas.js
@@ -0,0 +1,5 @@
+function nome() {
+    let nome = prompt('Qual é o seu nome?');
+    let idade = Number(prompt('Quantos anos você tem?'));
+    alert(`Acabei de conhecer ${nome.toUpperCase()}, que tem ${idade} anos de idade!`);
+}
