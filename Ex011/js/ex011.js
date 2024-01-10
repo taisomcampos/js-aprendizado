a28f7c38ed5e601ca0cc2d43f0f164004932e32e
Taisom Campos <taisomcampos@outlook.com>
2024-01-10 01:23:47 -0300

    Adicionando o Ex011

---
+++ Ex011/js/ex011.js
@@ -0,0 +1,16 @@
+function hora() {
+    let hora = document.querySelector('input#hora');
+    let res = document.querySelector('div#res');
+    let agora = new Date();
+    let horaAtual = agora.getHours();
+    res.innerHTML = `<p>Agora é/são <strong>${horaAtual} hora(s).</strong></p>`;
+    if (horaAtual < 5) {
+        res.innerHTML += `<p>Boa madrugada!</p>`;
+    } else if (horaAtual < 12) {
+        res.innerHTML += `<p>Bom dia!</p>`;
+    } else if (horaAtual <= 18) {
+        res.innerHTML += `<p>Boa tarde!</p>`;
+    } else {
+        res.innerHTML += `<p>Boa noite!</p>`;
+    }
+}
