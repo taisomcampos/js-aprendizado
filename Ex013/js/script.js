638cb899124433dba50e607bbc5e5c4303604ac1
Taisom Campos <taisomcampos@outlook.com>
2024-01-29 23:52:12 -0300

    Atualizando o  Ex013

--- Ex013/js/script.js
+++ Ex013/js/script.js
@@ -1,23 +1,20 @@
 function carregar() {
-    let texto = document.querySelector('div#texto');
-    let imagem = document.querySelector('img#imagem');
     let data = new Date();
-    let hora = data.getHours();
-    texto.innerHTML = `Agora são ${hora} horas.`;
+    let hora = 11 //data.getHours();
+    let texto = document.querySelector('div#txt');
+    let foto = document.querySelector('img#imagem');
+    texto.innerHTML = `Agora são ${hora} hora(s)!`;
     if (hora >= 0 && hora <= 12) {
-        document.body.style.background = '#acbe92';
-        texto.innerHTML += `<p>Bom dia!</p>`
-        texto.style.color = '#acbe92';
+        texto.innerHTML += `<p>Bom dia!</p>`;
         imagem.src = 'images/fotomanha.png';
+        document.body.style.background = '#acbe92';
     } else if (hora > 12 && hora <= 18) {
-        document.body.style.background = '#ce966d';
         texto.innerHTML += `<p>Boa tarde!</p>`;
-        texto.style.color = '#ce966d';
         imagem.src = 'images/fototarde.png';
+        document.body.style.background = '#ce966d';
     } else {
+        texto.innerHTML += `<p>Boa noite</p>`;
+        imagem.src = 'images/fotonoite.png';
         document.body.style.background = '#11699c';
-        texto.innerHTML += `<p>Boa noite!</p>`;
-        texto.style.color = '#11699c'
-        imagem.src = 'images/fotonoite.png'
     }
-}
+} 
