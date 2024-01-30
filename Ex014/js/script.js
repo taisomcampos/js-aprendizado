cc4216243ca7c07bfa1084be98f364ba04ea92e6
Taisom Campos <taisomcampos@outlook.com>
2024-01-30 12:36:16 -0300

    Atualizando o Ex014

--- Ex014/js/script.js
+++ Ex014/js/script.js
@@ -1,49 +1,49 @@
 function verificar() {
     let data = new Date();
     let ano = data.getFullYear();
-    let anoNascimento = document.querySelector('input#anoNascimento');
-    let resultado = document.querySelector('div#resultado');
-    if (anoNascimento.value == 0 || Number(anoNascimento.value) > ano) {
-        alert('[ERRO] Digite uma data válida!');
+    let anoNascimento = document.querySelector('input#nasc');
+    let resultado = document.querySelector('div#res');
+    if (anoNascimento.value == 0 || anoNascimento.value > ano) {
+        alert('[ERRO] Digite uma data válida!')
     } else {
-        let sexo = document.getElementsByName('sexo');
-        let idade = ano - Number(anoNascimento.value);
+        let sexo = document.getElementsByName('sex');
+        let idade = ano - anoNascimento.value;
         let genero = '';
         let imagem = document.createElement('img');
-        imagem.setAttribute('id', 'foto');
+        imagem.setAttribute('id', 'Foto do meu docucumento');
         if (sexo[0].checked) {
             genero = 'homem';
-            if (idade >= 0 && idade < 10) {
+            if (idade > 0 && idade < 10) {
                 // Criança
-                imagem.setAttribute('src', 'images/foto-crianca-masculino.png');
+                imagem.setAttribute('src', 'images/crianca-m.png');
             } else if (idade < 21) {
                 // Jovem
-                imagem.setAttribute('src', 'images/foto-jovem-masculino.png')
+                imagem.setAttribute('src', 'images/jovem-m.png');
             } else if (idade < 50) {
                 // Adulto
-                imagem.setAttribute('src', 'images/foto-adulto-masculino.png');
+                imagem.setAttribute('src', 'images/adulto-m.png')
             } else {
                 // Idoso
-                imagem.setAttribute('src', 'images/foto-idoso-masculino.png');
+                imagem.setAttribute('src', 'images/idoso-m.png');
             }
         } else {
             genero = 'mulher';
-            if (idade >= 0 && idade < 10) {
+            if (idade > 0 && idade < 10) {
                 // Criança
-                imagem.setAttribute('src', 'images/foto-crianca-feminino.png');
+                imagem.setAttribute('src', 'images/crianca-f.png');
             } else if (idade < 21) {
                 // Jovem
-                imagem.setAttribute('src', 'images/foto-jovem-feminino.png');
+                imagem.setAttribute('src', 'images/jovem-f.png');
             } else if (idade < 50) {
                 // Adulto
-                imagem.setAttribute('src', 'images/foto-adulto-feminino.png');
+                imagem.setAttribute('src', 'images/adulto-f.png')
             } else {
                 // Idoso
-                imagem.setAttribute('src', 'images/foto-idoso-feminino.png');
+                imagem.setAttribute('src', 'images/idoso-f.png');
             }
         }
+        resultado.innerHTML = `Detectamos <strong>${genero.toUpperCase()}</strong> de <strong>${idade} ano(s)</strong>.`
         resultado.style.textAlign = 'center';
-        resultado.innerHTML = `Detectamos ${genero} de ${idade} ano(s).`;
         resultado.appendChild(imagem);
     }
 }
