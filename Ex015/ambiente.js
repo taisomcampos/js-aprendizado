2a38677c1ff075ab7af238c775ce097fcae8e520
Taisom Campos <taisomcampos@outlook.com>
2024-02-05 20:49:01 -0300

    Atualizando o Ex015

--- Ex015/ambiente.js
+++ Ex015/ambiente.js
@@ -1,7 +1,11 @@
-let n = 0;
-let x = 0;
-while (n < 3) {
-    n++;
-    x += n;
-    console.log(`${n} ${x}`);
+let count = 1;
+while (count <= 10) {
+    console.log(`${count}`);
+    count++;
 }
+
+/*
+Para executar uma instrução while, o interpretador primeiramente avalia a expressão. Se o valor 
+da expressão é falso, o interpretador pula a instrução que serve de corpo do laço e vai para a 
+instrução seguinte no programa.
+*/
