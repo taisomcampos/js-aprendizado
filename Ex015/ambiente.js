d6399e23ea9b955d02f031cbe368c51d608f5542
Taisom Campos <taisomcampos@outlook.com>
2024-02-08 22:55:38 -0300

    Atualizando o ambiente.js do Ex015

--- Ex015/ambiente.js
+++ Ex015/ambiente.js
@@ -5,7 +5,6 @@ while (count <= 10) {
 }
 
 /*
-Para executar uma instrução while, o interpretador primeiramente avalia a expressão. Se o valor 
-da expressão é falso, o interpretador pula a instrução que serve de corpo do laço e vai para a 
+Para executar uma instrução while, o interpretador primeiramente avalia a expressão. Se o valor da expressão é falso, o interpretador pula a instrução que serve de corpo do laço e vai para a 
 instrução seguinte no programa.
 */
