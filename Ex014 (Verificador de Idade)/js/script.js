86b881558eb93d25f23ad4077de62ee40e183959
Taisom Campos <taisomcampos@outlook.com>
2024-02-29 21:19:25 -0300

    Revisando o Ex Verificador de Idade

--- Ex014 (Verificador de Idade)/js/script.js
+++ Ex014 (Verificador de Idade)/js/script.js
@@ -1,10 +1,10 @@
 function verificar() {
     let agora = new Date();
     let ano = agora.getFullYear();
-    let anoNascimento = document.querySelector('input#nasc');
-    let texto = document.querySelector('div#res');
+    let anoNascimento = document.querySelector('input#txtano');
+    let resultado = document.querySelector('div#res');
     if (anoNascimento.value == 0 || anoNascimento.value > ano) {
-        alert('[ERRO] Digite uma data válida!');
+        alert('[ERRO!] Digite um número válido!');
     } else {
         let sexo = document.getElementsByName('sxo');
         let idade = ano - anoNascimento.value;
@@ -19,7 +19,7 @@ function verificar() {
             } else if (idade < 21) {
                 //jovem
                 foto.setAttribute('src', '/Ex014 (Verificador de Idade)/images/jovem-m.png');
-            } else if (idade < 50) {
+            } else if (idade < 50){
                 //adulto
                 foto.setAttribute('src', '/Ex014 (Verificador de Idade)/images/adulto-m.png');
             } else {
@@ -34,7 +34,7 @@ function verificar() {
             } else if (idade < 21) {
                 //jovem
                 foto.setAttribute('src', '/Ex014 (Verificador de Idade)/images/jovem-f.png');
-            } else if (idade < 50) {
+            } else if (idade < 50){
                 //adulto
                 foto.setAttribute('src', '/Ex014 (Verificador de Idade)/images/adulto-f.png');
             } else {
@@ -42,8 +42,8 @@ function verificar() {
                 foto.setAttribute('src', '/Ex014 (Verificador de Idade)/images/idoso-f.png');
             }
         }
-        texto.innerHTML = `Detectamos <strong>${genero.toUpperCase()}</strong> de <strong>${idade} ano(s)</strong>`;
-        texto.style.textAlign = 'center';
-        texto.appendChild(foto);
+        resultado.innerHTML = `Detectamos <strong>${genero.toUpperCase()}</strong> de <strong>${idade}</strong> ano(s).`;
+        resultado.style.textAlign = 'center';
+        resultado.appendChild(foto)
     }
 }
